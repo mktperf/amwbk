baa6912e7cb3c3daafbb73a4c86b836e5ae6a2d6
mktperf <mktfacemi.perf@gmail.com>
2025-03-20 18:23:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Token de verificação do Facebook
-VERIFY_TOKEN = "MEU_TOKEN_SECRETO"
+VERIFY_TOKEN = "sec-123-@1"
 
 @app.route('/webhook', methods=['GET'])
 def verify():
